556af30d2ff66868053124d2767f2ce84161a8a5
baztian <bastian.dev@gmail.com>
2017-06-02 13:16:06 +0200

    Add dockerfile for wildfly. Remove unnecessary stuff.

--- src/main/java/me/ejb/mini/MyTriggerResource.java
+++ src/main/java/me/ejb/mini/MyTriggerResource.java
@@ -1,32 +1,18 @@
-
 package me.ejb.mini;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Stateless
-@Path ("mytrigger")
+@Path("mytrigger")
 public class MyTriggerResource {
 
-    @Inject
-    ScopedBean bean;
-
-    @Inject
-    AsyncContextUser async;
-
-
-    @GET
-    public String doit() throws Exception {
-        StringBuilder builder = new StringBuilder("Synchronous: ");
-        builder.append(bean.getCount());
-        builder.append(";");
-        builder.append(bean.increment().getCount());
-        async.printCount();
-        builder.append(";");
-        builder.append(bean.increment().getCount());
-        return builder.toString();
-    }
+  @GET
+  public JsonObject doit() throws Exception {
+    return Json.createObjectBuilder().add("value", 42).build();
+  }
 
 }
